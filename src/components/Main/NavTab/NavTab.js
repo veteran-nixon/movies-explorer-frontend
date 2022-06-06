dedf4636a7a62ef00a1a51e40ae34c18bb85bde1
Evgeny Dolganev <dolganev007@yandex.ru>
2022-06-06 15:41:29 +0700

    Создать компоненты Promo и NavTab

---
+++ src/components/Main/NavTab/NavTab.js
@@ -0,0 +1,7 @@
+function NavTab(props) {
+  return (
+    <a className="nav-tab__link">{props.text}</a>
+  )
+}
+
+export default NavTab;
--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,9 +1,13 @@
+import NavTab from "../NavTab/NavTab";
+
 function Promo() {
   return (
     <section className="promo">
-      <h1>Учебный проект студента факультета Веб-разработки.</h1>
-      <div className="nav-tab">
-
+      <h1 className="promo__heading">Учебный проект студента факультета Веб-разработки.</h1>
+      <div className="promo__nav-container">
+        <NavTab text='О проекте' />
+        <NavTab text='Технологии' />
+        <NavTab text='Студент' />
       </div>
     </section>
   )
